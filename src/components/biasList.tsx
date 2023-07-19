d9e04755764c945be8f6393702d6419496f9007f
xavier-kong <kongxianchengxavier@gmail.com>
2023-07-19 20:29:38 +0800

    working with table

--- src/components/biasList.tsx
+++ src/components/biasList.tsx
@@ -1,5 +1,6 @@
 import type { inferRouterOutputs } from "@trpc/server";
 import type { AppRouter } from "~/server/api/root";
+import Image from 'next/image';
 
 type RouterOutput = inferRouterOutputs<AppRouter>;
 type BiasQueryOutput = RouterOutput['user']['fetchUserBiases'];
@@ -13,9 +14,9 @@ function BiasList({ biases, emptyMessage }: { biases: BiasQueryOutput | undefine
     const userBiases = biases.userBiases;
 
     return (
-        <table className="table-auto">
+        <table className="table-auto border-separate border-spacing-x-4">
             <thead>
-                <tr>
+                <tr className="text-left">
                     <th>Group</th>
                     <th>Member</th>
                 </tr>
@@ -25,7 +26,10 @@ function BiasList({ biases, emptyMessage }: { biases: BiasQueryOutput | undefine
                     userBiases.map(bias => {
                         return (
                             <tr key={bias.memberId}>
-                                <td>{bias.member.group.enName}</td>
+                                <td>
+                                    <Image src={bias.member.group.logoUrl} alt={`logo for ${bias.member.group.enName}`}/>
+                                    {bias.member.group.enName}
+                                </td>
                                 <td>{bias.member.enName}</td>
                             </tr>
                         )
